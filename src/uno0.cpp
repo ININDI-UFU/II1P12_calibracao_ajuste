086432b2d280b8aadf40298165b939049b4d739b
josuemoraishg <josue@ufu.br>
2026-07-15 07:40:15 -0300

    refactor caract

--- src/uno0.cpp
+++ src/uno0.cpp
@@ -1,12 +1,14 @@
  #include <Arduino.h>
- 
- #define pinANALOG A5 // Configura o pino de leitura
- 
+
+ #define pinANALOG 34 // GPIO34 (ADC1_CH6) - pino somente-entrada, ideal para leitura analógica no ESP32
+
  uint32_t timeDelayMS = 10;
  uint32_t expiresDelayMS = 0;
- 
+
  void setup() {
-     Serial.begin(9600);
+     Serial.begin(115200);
+     analogReadResolution(12);        // ADC de 12 bits (0-4095)
+     analogSetAttenuation(ADC_11db);  // Faixa de leitura de 0 a ~3.3V
  }
  
  void loop() {
