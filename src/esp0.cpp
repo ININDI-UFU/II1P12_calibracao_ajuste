32df07b0ca28acaad7794c2f3a51f2bdf5bcc840
josuemoraishg <josue@ufu.br>
2026-07-21 07:39:43 -0300

    refactor ininid

--- src/esp0.cpp
+++ src/esp0.cpp
@@ -3,9 +3,9 @@
 #include <freertos/task.h>
 #include <freertos/queue.h>
 
-#define GAIN 1
-#define OFFSET 0
-#define pinANALOG A5 // Configura o pino de leitura
+#define GAIN 1             // Ganho para calibrar o valor do ADC (em unidades de leitura do ADC)
+#define OFFSET 0           // Offset para calibrar o valor do ADC (em unidades de leitura do ADC)
+#define pinANALOG A5       // Configura o pino de leitura
 #define SAMPLE_PERIOD_MS 1 // período de amostragem da task (1 kHz)
 
 static QueueHandle_t adcQueue = nullptr;
@@ -22,7 +22,7 @@ static void adcSampleTask(void *pvParameters) {
 }
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
     adcQueue = xQueueCreate(64, sizeof(uint16_t));
     xTaskCreate(adcSampleTask, "adcSampleTask", 2048, nullptr, 1, nullptr);
 }
